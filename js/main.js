ea5fc1257bdc683f1865fd9a33a7f9ebe4a4ac88
lucas97-png <74870937+lucas97-png@users.noreply.github.com>
2021-10-12 22:54:28 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,62 @@ $('.features-carousel').owlCarousel({
         }
     }
 })
+
+
+$('.screenshots-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:4,
+        }
+    }
+})
+
+$('.testimonials-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+})
+
+$('.team-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+})
+
+$.scrollIt({
+    topOffset: -50
+})
